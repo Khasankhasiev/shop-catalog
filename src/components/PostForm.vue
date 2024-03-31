adf447343f0e47703b39ab6899620205bcb0c4f2
Khasan <mr.khasiev@list.ru>
2024-03-31 03:15:50 +0300

    created new ui components

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,13 +2,13 @@
   <div>
     <form @submit.prevent>
       <h4>Создание поста</h4>
-      <input
+      <my-input
         v-model="post.title"
         class="input"
         type="text"
         placeholder="Название поста"
       />
-      <input
+      <my-input
         v-model="post.body"
         class="input"
         type="text"
@@ -50,11 +50,5 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
 }
 </style>
