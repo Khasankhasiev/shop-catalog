be188aea37223d18ab21772306748d4f2b933bdb
Khasan <mr.khasiev@list.ru>
2024-04-14 15:38:20 +0300

    created page navigation using vue-router

---
+++ src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Данная страница была создана в целях обучения фреймворку VUE</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,1 +1,27 @@
+import Main from "@/pages/Main";
+import { createRouter, createWebHistory } from "vue-router";
+import PostPage from "@/pages/PostPage.vue";
+import About from "@/pages/About.vue";
 
+const routes = [
+  {
+    path: "/",
+    component: Main,
+  },
+  {
+    path: "/posts",
+    component: PostPage,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(process.env.BASE_URL),
+});
+
+// app.use(router);
+export default router;
