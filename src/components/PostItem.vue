9786fca62a4d4465ed54709b5db81a466c038bbc
Khasan <mr.khasiev@list.ru>
2024-04-15 23:11:49 +0300

    added fucntionality to open posts

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -6,7 +6,9 @@
       <div style="margin: 10px"><strong>Описание:</strong>{{ post.body }}</div>
     </div>
     <div class="post__btns">
-      <my-button>Открыть</my-button>
+      <my-button @click="$router.push(`/posts/${post.id}`)">
+        Открыть
+      </my-button>
       <my-button @click="$emit('remove', post)">Удалить</my-button>
     </div>
   </div>
